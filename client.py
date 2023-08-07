9686e7e8210e2c02bd0298e3c3e6787dbb63c258
Rubens Lima <rubensndelima@hotmail.com>
2023-08-07 11:26:58 -0300

    tabela de aprendizado atualizada

--- client.py
+++ client.py
@@ -9,6 +9,7 @@ RF = 18  # Constante de aleatoriedade
 
 # Lista de Movimentos
 dict_mov = ['left', 'right', 'jump']
+dict_direcao  = ['N', 'L', 'S', 'O']
 
 # Funcao para obter a posicao na matriz
 def getPosition(string:str)->int:
@@ -20,7 +21,7 @@ def getPosition(string:str)->int:
 def getNextMove(bloco, matrix)->float:
     # Verfico se vai fazer uma transicao aleatoria ou não
     if random.randint(0,100) < RF:
-        return random.randint(0,2)
+        return random.randint(0,2)   
     # Pego o maior valor que possuo na tabela para deteminado bloco
     valores_blocos = [matrix[i][bloco] for i in range(3)]
     return valores_blocos.index(max(valores_blocos))
@@ -28,6 +29,15 @@ def getNextMove(bloco, matrix)->float:
 def qLearning(matrix, direcaoAtual, direcaoNova, blocoAtual, blocoNovo)->float:
     return  (matrix[direcaoAtual][blocoAtual] + 
              LR * (reward + DF * matrix[direcaoNova][blocoNovo] - matrix[direcaoAtual][blocoAtual]))
+
+def printBlocosValue():
+    print('|Bloco-pos|left |right|jump |')
+    for plataforma in sorted(plataformas_visitadas):
+        offset = plataforma%4
+        plataforma, pos = plataforma//4, dict_direcao[offset]
+        print('|{:<4}{:<5}|{:<5}|{:<5}|{:<5}|'.format(
+            plataforma, pos, *[round(matrix[i][plataforma],3) for i in range(3)]
+        ))
 
 if __name__ == "__main__":
     # Quantidade de Movimentos e Sucessos
@@ -39,6 +49,7 @@ if __name__ == "__main__":
 
     # Lendo a Matriz pré salva
     matrix = pd.read_csv('resultado.txt', header = None, sep=' ')
+    plataformas_visitadas = set({})
 
     # Conectando com o servidor
     appConnect = cn.connect(2037)         
@@ -46,7 +57,7 @@ if __name__ == "__main__":
     if appConnect:
         #for i in range(N_LOOPS):
         i = 1
-        while success < 10:
+        while success < 10000:
             action = dict_mov[direcaoAtual]
             print(60*"-" + f'\nI:{i}, A: {action}')
             # Envio para o servidor a acao escolhida
@@ -59,10 +70,12 @@ if __name__ == "__main__":
             # Atualizo o valor da matrix para aquele bloco utlizando o calculo QL
             matrix[direcaoAtual][plataformaAtual] = qLearning(matrix, direcaoAtual, direcaoNova, 
                                                               plataformaAtual, plataformaNova)
+            plataformas_visitadas = plataformas_visitadas | {plataformaAtual}
+            printBlocosValue()
             # Atualizo as novas direcoes e plataforma
             direcaoAtual, plataformaAtual = direcaoNova, plataformaNova
             i +=1
+            matrix.to_csv('resultado.txt', header=None, index=None, mode='w', sep = ' ')
         
         print(f'total success: {success}')
-        matrix.to_csv('resultado.txt', header=None, index=None, mode='w', sep = ' ')
         appConnect.close()
